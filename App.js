3940126ecebfc68c3998b6d23e3f18649b67f17c
liamca1 <88624211+liamca1@users.noreply.github.com>
2022-06-18 14:58:27 +0200

    5.5 — send message not functioning..

--- App.js
+++ App.js
@@ -8,18 +8,22 @@ import 'react-native-gesture-handler';
 // import react Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Component } from 'react';
 
 // Create the navigator
 const Stack = createStackNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-    <Stack.Navigator initialRouteName='Start'>
-      <Stack.Screen name='Start' component={Start} />
-      <Stack.Screen name='Chat' component={Chat} />
-    </Stack.Navigator>
+export default class App extends Component {
 
-  </NavigationContainer>
-);
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Start'>
+          <Stack.Screen name='Start' component={Start} />
+          <Stack.Screen name='Chat' component={Chat} />
+        </Stack.Navigator>
+
+      </NavigationContainer>
+    );
+  }
 }
